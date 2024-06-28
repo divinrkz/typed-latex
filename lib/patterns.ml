ff0cc758098c9f83df87a47add84d379aa527b4a
Gosha Lubashev <bluecapacitor@gmail.com>
2024-06-28 11:21:02 -0700

    finished main recursive loop for pattern matching; wrote list_relation_types_pattern and other pattern helpers

--- lib/patterns.ml
+++ lib/patterns.ml
@@ -19,6 +19,11 @@ type relation_type =
   | Other
 [@@deriving eq, show, sexp, hash, ord]
 
+let all_known_relation_types =
+  [ Le; Leq; Ge; Geq; Eq; In; NotIn; Subset; Superset; SubsetEq; SupersetEq ]
+
+let all_relation_types = Other :: all_known_relation_types
+
 type elementary_relation = ERelation of relation_type * Math.t * Math.t
 
 let ast_to_elementary_relation_type (ast_rel_t : Math.relation) =
@@ -42,25 +47,9 @@ let rec extract_elementary_relations (bound : Math.t)
   | (ast_rel_t, right) :: (_, left) :: remaining ->
       ERelation (ast_to_elementary_relation_type ast_rel_t, left, right)
       :: extract_elementary_relations bound remaining
-  | (ast_rel_t, right) :: [] -> [ERelation (ast_to_elementary_relation_type ast_rel_t, bound, right)]
+  | (ast_rel_t, right) :: [] ->
+      [ ERelation (ast_to_elementary_relation_type ast_rel_t, bound, right) ]
   | [] -> []
-
-let rec relation_simplified_eq (relation : relation_type) (ast_rel : Math.t) =
-  match (relation, ast_rel) with
-  | Le, Math.Relation (_, (Math.Le, _) :: _) -> true
-  | Leq, Math.Relation (_, (Math.Leq, _) :: _) -> true
-  | Ge, Math.Relation (_, (Math.Ge, _) :: _) -> true
-  | Geq, Math.Relation (_, (Math.Geq, _) :: _) -> true
-  | Eq, Math.Relation (_, (Math.Eq, _) :: _) -> true
-  | In, Math.Relation (_, (Math.In, _) :: _) -> true
-  | NotIn, Math.Relation (_, (Math.NotIn, _) :: _) -> true
-  | Subset, Math.Relation (_, (Math.Subset, _) :: _) -> true
-  | Superset, Math.Relation (_, (Math.Superset, _) :: _) -> true
-  | SubsetEq, Math.Relation (_, (Math.SubsetEq, _) :: _) -> true
-  | SupersetEq, Math.Relation (_, (Math.SupersetEq, _) :: _) -> true
-  | relation, Math.Relation (bound_var, _ :: rem_bindings) ->
-      relation_simplified_eq relation (Math.Relation (bound_var, rem_bindings))
-  | _ -> false
 
 module MatchID = struct
   module T = struct
@@ -82,22 +71,33 @@ type pattern =
   | Repeat of pattern
   | TypeName of MatchID.t
   | DefContainer of pattern * MatchID.t
-  | Relation of relation_type * MatchID.t * MatchID.t
+  | Relation of relation_type * MatchID.t
   (* Auxiliary *)
   | OptRepeat of pattern
+
+let list_relation_types_pattern (match_id : MatchID.t)
+    (allowed_relation_types : relation_type list) =
+  Any
+    ((fun rel_type -> Relation (rel_type, match_id)) |<<: allowed_relation_types)
+
+let any_known_relation_pattern (match_id : MatchID.t) =
+  list_relation_types_pattern match_id all_known_relation_types
+
+let any_relation_pattern (match_id : MatchID.t) =
+  list_relation_types_pattern match_id all_relation_types
 
 let def =
   Sequence
     [
       Any [ Word "choose"; Word "consider"; Word "define" ];
-      Relation (Eq, MatchID.from_int 1, MatchID.from_int 2);
+      Relation (Eq, MatchID.from_int 1);
     ]
 
 module rec MatchContainer : sig
   type value =
     | DefContainerMatch of t
     | TypeNameMatch of string
-    | ExpressionMatch of Math.t
+    | RelationMatch of elementary_relation
 
   and t = value MatchID.Map.t
 
@@ -107,7 +107,7 @@ end = struct
   type value =
     | DefContainerMatch of t
     | TypeNameMatch of string
-    | ExpressionMatch of Math.t
+    | RelationMatch of elementary_relation
 
   and t = value MatchID.Map.t
 
@@ -142,6 +142,20 @@ let box_type_name (match_id : MatchID.t) (exterior_matches : MatchContainer.t)
     ((name, tokens) : string * proof_token list) =
   (tokens, MatchContainer.put exterior_matches match_id (TypeNameMatch name))
 
+let rec find_elementary_relation (rel_type : relation_type)
+    (e_relations : elementary_relation list) =
+  match e_relations with
+  | (ERelation (found_type, _, _) as out_rel) :: _
+    when equal_relation_type found_type rel_type ->
+      [ out_rel ]
+  | _ :: remainder -> find_elementary_relation rel_type remainder
+  | [] -> []
+
+let box_relation (match_id : MatchID.t) (tokens : proof_token list)
+    (exterior_matches : MatchContainer.t) (e_relation : elementary_relation) =
+  ( tokens,
+    MatchContainer.put exterior_matches match_id (RelationMatch e_relation) )
+
 let rec match_rec (pat : pattern) (context : context) =
   match (pat, context) with
   | Word p_word, (WordToken word :: remainder, matches)
@@ -159,62 +173,10 @@ let rec match_rec (pat : pattern) (context : context) =
   | DefContainer (p, match_id), (tokens, exterior_matches) ->
       let interior_contexts = match_rec p (tokens, MatchContainer.empty) in
       box_context match_id exterior_matches |<<: interior_contexts
-  (* | ( Relation (relation_type, left_id, right_id),
-      (MathToken math :: remainder, exterior_matches) ) when ->
-      [] *)
+  | ( Relation (relation_type, match_id),
+      ( MathToken (Math.Relation (ast_head, ast_relations)) :: remainder,
+        exterior_matches ) ) ->
+      let e_relations = extract_elementary_relations ast_head ast_relations in
+      box_relation match_id remainder exterior_matches
+      |<<: find_elementary_relation relation_type e_relations
   | _ -> []
-
-(* type result_t = (int, Math.t) Hashtbl.t *)
-
-(* let match_with (pat : pattern) (latex : Latex.t) =
-   let (mappings : result_t) = Hashtbl.create (module Int) in
-   let rec recurse (pat : pattern) (node : Latex.t) =
-     match (pat, node) with
-     | Word y, { pos = _; value = Word x } ->
-         String.equal (String.lowercase x) (String.lowercase y)
-     | Option p, { pos = _; value = _ } -> recurse p node
-     (* ignore whitespace and newlines *)
-     | _, { pos = p; value = Latex ({ pos = _; value = Whitespace } :: tl) } ->
-         recurse pat { pos = p; value = Latex tl }
-     | _, { pos = p; value = Latex ({ pos = _; value = Newline } :: tl) } ->
-         recurse pat { pos = p; value = Latex tl }
-     (* match sequences *)
-     | Sequence [], { pos = _; value = _ } -> true
-     | Sequence (fst :: rest), { pos = p; value = Latex (hd :: tl) } ->
-         if
-           (* Format.printf "Matching %a with %s\n" Latex.pp node (show_pattern pat); *)
-           recurse fst hd
-         then recurse (Sequence rest) { pos = p; value = Latex tl }
-         else false
-     | Any [], { pos = _; value = _ } -> false
-     | Any (hd :: tl), { pos = _; value = _ } ->
-         if recurse hd node then true else recurse (Any tl) node
-     | Variable id, { pos = _; value = Mathmode x } -> (
-         let result_t = parse_math (Mathmode x) in
-         match result_t with
-         | [ Variable t ] ->
-             Hashtbl.set mappings ~key:id ~data:(Variable t);
-             true
-         | _ -> false)
-     | Relation id, { pos = _; value = Mathmode x } -> (
-         let result_t = parse_math (Mathmode x) in
-         match result_t with
-         | [ (Relation _ as ast_rel) ] ->
-             Hashtbl.set mappings ~key:id ~data:ast_rel;
-             true
-         | _ -> false)
-     | SpecificRelation (id, relation), { pos = _; value = Mathmode x } -> (
-         let result_t = parse_math (Mathmode x) in
-         match result_t with
-         | [ (Relation _ as ast_rel) ]
-           when relation_simplified_eq relation ast_rel ->
-             Hashtbl.set mappings ~key:id ~data:ast_rel;
-             true
-         | _ -> false)
-     (* | _, { pos = _; value = Environment (_, _, )} *)
-     | _ ->
-         Format.printf "Could not match %a with %s\n" Latex.pp node
-           (show_pattern pat);
-         false
-   in
-   if recurse pat latex then Some mappings else None *)
