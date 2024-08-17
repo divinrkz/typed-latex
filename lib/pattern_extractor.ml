2886a9acd36a368e74d50958935e5c433533a99e
divinrkz <dirakiza@caltech.edu>
2024-08-16 22:08:14 -0700

    feat: 80% working patterns

--- lib/pattern_extractor.ml
+++ lib/pattern_extractor.ml
@@ -5,13 +5,11 @@ open Util
 
 module PatternDef: sig  
   type t
-
-
+  
   val constr: (int * string * pattern) -> t
   val get_id: t -> int
   val get_line: t -> string
   val get_pattern: t -> pattern
-  (* val tree_format: t -> string *)
 end = struct 
     type t = {
       id: int;
@@ -20,14 +18,11 @@ end = struct
     }
     [@@deriving eq, show, sexp, hash, ord, compare]
 
-
   let constr (id, line, pattern) = { id; line; pattern }
-
   let get_id t = t.id
   let get_line t = t.line
   let get_pattern t = t.pattern
 end
-
 
 module PatternExtractor = struct
   type t = {
@@ -47,14 +42,8 @@ module PatternExtractor = struct
     incr curr_match_id;
     !curr_match_id
 
-  let get_match_id () = 
+  let get_id () = 
     MatchID.from_int (gen_id ())
-
-  let regex_first_split = "|?[a-zA-Z]+|?"
-  let regex_optional = "\\(([^()]+)\\)\\?"
-  let relation_regex = "[a-zA-Z]+"
-
-(* Example usage *)
 
   let parse_relation_type (relation_type: string) = 
     match relation_type with
@@ -66,58 +55,8 @@ module PatternExtractor = struct
     | "Mgreater_than_or_equal" -> "GreaterThan"
     | "Mless_than" -> "StrictLessThan"
     | "Mless_than_or_equal" -> "LessThan"
+    | "Mvar" -> "Variable"
     | _ -> "Other"
-
-  let parse_relations relations_str =
-    let def_container = ref [] in
-    let relation_splits = Util.String.split relations_str ' ' in
-    let rec parse_relation splits =
-      match splits with
-      | [] -> !def_container
-      | relation :: rest ->
-        let parsed_relation = DefContainer (MathPattern (Function (parse_relation_type relation, [Expression (get_match_id ()); Expression (get_match_id ())], (get_match_id ()))), (get_match_id ())) in
-        def_container := !def_container @ [parsed_relation];
-        parse_relation rest
-    in
-    let relations = parse_relation relation_splits in
-      match relations with 
-      | _ ->  [Any relations]
-  
-  let parse_words words_str seq =
-    let word_splits = Util.String.split words_str ' ' in
-    let rec process_words words acc =
-      
-      match words with
-      | [] -> acc
-      | [str] -> acc @ [Word str]
-      | str1 :: str2 :: rest ->
-        let stripped_str1 = if str_ends_with str1 "|" then String.sub str1 ~pos:0 ~len:(String.length str1 - 1) else str1 in
-        let stripped_str2 = if str_ends_with str2 "|" then String.sub str2 ~pos:0 ~len:(String.length str2 - 1) else str2 in
-        if str_ends_with str1 "|" || str_ends_with str2 "|" then
-          acc @ [Any [Word stripped_str1; Word stripped_str2]] @ process_words rest [] 
-        else
-          acc @ [Word str1] @ process_words (str2 :: rest) []
-        in
-      let rec parse_word splits acc =
-          match splits with
-          | [] -> acc
-          | word :: rest ->
-            let optional_matches = Util.regex_matcher word regex_optional in
-            let new_acc =
-              match optional_matches with
-              | [opt] -> 
-                let stripped_opt = Util.regex_first_matcher opt regex_first_split in 
-                  acc @ [Optional (Word stripped_opt)]
-              | _ -> 
-                let matched_lst = Util.regex_matcher word regex_first_split in
-                match matched_lst with
-                | [] -> acc
-                | [str] -> acc @ [Word str]
-                | _ -> process_words matched_lst acc
-            in
-            parse_word rest new_acc
-        in
-        seq := parse_word word_splits !seq
   
 let extract_first_split first_split = 
   let words = Util.String.split first_split ' ' in
@@ -146,7 +85,7 @@ let extract_first_split first_split =
         if start_pos < end_pos then
           String.sub ~pos:start_pos ~len:(end_pos - start_pos) word
         else
-          word  (* Return original word if positions are not valid *)
+          word 
       else
         word
     in
@@ -165,15 +104,27 @@ let extract_first_split first_split =
       if is_valid_word word &&  String.length word > 0 then
         if is_optional then
           if Char.equal (String.get word 0) 'M' then
-            let relation_type = DefContainer (MathPattern (Function (parse_relation_type word, [Expression (get_match_id ()); Expression (get_match_id ())], (get_match_id ()))), (get_match_id ())) in
+            let parsed_relation = parse_relation_type word in 
+            let relation_type = 
+              (match parsed_relation  with 
+                | "Variable" -> DefContainer (MathPattern (Expression (get_id ())), (get_id ()))
+                | _ -> DefContainer (MathPattern (Function (parsed_relation, 
+                [Expression (get_id ()); Expression (get_id ())], (get_id ()))), (get_id ()))
+            ) in 
+
             Optional relation_type
+          else if String.equal word "D" then
+            let df = DefContainer (MathPattern (Expression (get_id ())), (get_id ())) in 
+            Optional (df)
           else Optional (Word word)
         else 
           if Char.equal (String.get word 0) 'M' then
-            DefContainer (MathPattern (Function (parse_relation_type word, [Expression (get_match_id ()); Expression (get_match_id ())], (get_match_id ()))), (get_match_id ()))
+            DefContainer (MathPattern (Function (parse_relation_type word, [Expression (get_id ()); Expression (get_id ())], (get_id ()))), (get_id ()))
+          else if String.equal word "D" then 
+            DefContainer (MathPattern (Expression (get_id ())), (get_id ()))
           else 
             if String.contains word '_' then 
-              Word (Util.String.replace_char_at_index word (String.index_exn word '_') ' ')
+              Word (Util.String. replace_char_at_index word (String.index_exn word '_') ' ')
             else
               Word word 
       else 
@@ -190,13 +141,10 @@ let extract_first_split first_split =
   (build_sequence words)
 
 
-
-
   let extract_patterns extractor filename =
     In_channel.with_file filename ~f:(fun input_c ->
       let line_counter = ref 0 in
       In_channel.iter_lines input_c ~f:(fun line ->
-
         let seq = ref [] in
 
         let segment_splits = Util.String.split line ':' in 
@@ -204,6 +152,8 @@ let extract_first_split first_split =
             let parsed = extract_first_split split in
             seq := !seq @ parsed
           );
+        
+        line_counter := !line_counter + 1;
         let extracted = PatternDef.constr (!line_counter, line, (Sequence !seq)) in
           add_pattern extractor extracted;
       );
@@ -220,9 +170,10 @@ let def =
       Optional (Sequence [ Word "we"; Word "could" ]);
       Any [ Word "suppose"; Word "say"; Word "imagine"; Word "let" ];
       DefContainer
-        ( MathPattern (Function ("StrictGreaterThan", [ Expression (PatternExtractor.get_match_id ()); Expression (PatternExtractor.get_match_id ()) ], (PatternExtractor.get_match_id ()))),
-          (PatternExtractor.get_match_id ()));
+        ( MathPattern (Function ("StrictGreaterThan", [ Expression (PatternExtractor.get_id ()); Expression (PatternExtractor.get_id ()) ], (PatternExtractor.get_id ()))),
+          (PatternExtractor.get_id ()));
     ]
 
 
 
+    
