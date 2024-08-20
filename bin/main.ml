59fdba9c7507060edaf42e7228ca9cde7f2e8be7
Gosha Lubashev <bluecapacitor@gmail.com>
2024-08-20 15:07:31 -0700

    fixed MatchID signature change issues

--- bin/main.ml
+++ bin/main.ml
@@ -6,22 +6,19 @@ open String_tree
 open Proof_lex
 open Pattern_extractor
 open Patterns
-
 module Json = Yojson.Basic
 
 let main () =
   let filename = "assets/json/parsed-latex.json" in
 
-  
-  let extractor = PatternExtractor.constr in 
-    PatternExtractor.extract_patterns extractor "assets/patterns/patterns.txt";
-  let extracted_patterns = PatternExtractor.get_patterns extractor in 
-    extracted_patterns >->: (fun pattern -> 
-      print_string ("\nLine [" ^ (string_of_int (PatternDef.get_id pattern)) ^ "]: ");
-      print_endline ("\"" ^ PatternDef.get_line pattern ^ "\"");
-      print_endline (show_pattern (PatternDef.get_pattern pattern))
-    ); 
-  
+  let extractor = PatternExtractor.constr in
+  PatternExtractor.extract_patterns extractor "assets/patterns/patterns.txt";
+  let extracted_patterns = PatternExtractor.get_patterns extractor in
+  ( extracted_patterns >->: fun pattern ->
+    print_string ("\nLine [" ^ string_of_int (PatternDef.get_id pattern) ^ "]: ");
+    print_endline ("\"" ^ PatternDef.get_line pattern ^ "\"");
+    print_endline (show_pattern (PatternDef.get_pattern pattern)) );
+
   let json = Json.from_file filename in
   let parsed_latex = RawLatex.deserialize_from_json json in
 
@@ -30,19 +27,17 @@ let main () =
   print_endline << RawLatex.tree_format <-<? document_ast;
   let pattern = Patterns.Word "TEST" in
   let tokenization = Proof_lex.tokenize |<<? document_ast in
-  
-  
+
   (fun token_streams ->
     print_endline
       ("\nFound "
       ^ string_of_int (List.length token_streams)
-      ^ " token stream(s)")
-      )
+      ^ " token stream(s)"))
   <-<? tokenization;
   tokenization
   >->? List.iter ~f:(fun token_stream ->
-           (* print_endline ("\n" ^ String.make 16 '='); *)
-           (* print_endline
+           print_endline ("\n" ^ String.make 16 '=');
+           print_endline
              (tree_format "| "
                 (Branch
                    ( Some
@@ -50,7 +45,7 @@ let main () =
                        ^ string_of_int (List.length token_stream)),
                      token_stream >>|: ProofToken.to_string_tree )));
            let matched_context = Patterns.match_pattern pattern token_stream in
-           (* print_endline
+           print_endline
              (if is_some matched_context then "\nMatched the pattern"
               else "\nDid not match the pattern");
            let matches = Triple.second |<<? matched_context in
